bd7969aebed702ac37bf1609d6d7d20e8781e913
jeor0980 <jesusotovar@hotmail.com>
2019-09-06 11:53:23 -0600

    vue js static files

--- src/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="py-4">
-    <!-- Please consider donating on the link below or keeping this on your page 🤘 -->
-    <p class="mb-0">Jesus is a software developer by day and bass power chord strummer by night, he also often likes to stay up late going to live rock shows.</p>
-    <br>
-    <p class="mb-0">Built with <a href="https://github.com/montoulieu/link-ent" target="_blank">LinkEnt</a></p>
-  </footer>
-</template>
