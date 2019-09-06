7d8feea773accdc236cef970450c950e37b51c04
jeor0980 <jesusotovar@hotmail.com>
2019-09-06 11:38:29 -0600

    Initial commit of vue app

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-## Copyright and License
+# links
 
-Copyright 2013-2016 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-creative/blob/gh-pages/LICENSE) license.
+> A customizable and free link-tree clone
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="list-inline mb-0">
+    <li v-for="account in socialAccounts" :key="account.id" class="list-inline-item">
+      <a :href="account.url" target="_blank">
+        <i :class="['fa fa-2x', 'fa-' + account.company]"></i>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      socialAccounts: [
+        // {
+        //   company: 'twitter', /* Used for font-awesome icon */
+        //   url: 'https://twitter.com/' /* URL for social account */
+        // },
+        {
+          company: 'twitter',
+          url: 'https://twitter.com/topo_95'
+        },
+        {
+          company: 'github',
+          url: 'https://github.com/jeor0980'
+        },
+        {
+          company: 'spotify',
+          url: 'https://open.spotify.com/user/12736438?si=NfzdFGdrTnmREzdc7CEBcg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
